32711fe8ed83ed98d5c700b72c6887212b133418
Justin Turner <jmturner638@gmail.com>
2021-08-02 07:36:37 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    name = input("Enter username:")
+    password = input("Enter password:")
+
+    if (username == "grace" and password == "hopper") or /
+        (username == "emma" and password == "haskell"):
+        print("You have succefffully logged in!")
+    else:
+        print("Incorrect username or password!")
 
 if __name__ == '__main__':
     main()
