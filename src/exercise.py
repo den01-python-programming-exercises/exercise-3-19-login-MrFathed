b7fa510e60168346c6cc1a1f41e35711a493788f
Justin Turner <jmturner638@gmail.com>
2021-08-02 07:39:01 -0500

    Second Try

--- src/exercise.py
+++ src/exercise.py
@@ -3,8 +3,7 @@ def main():
     name = input("Enter username:")
     password = input("Enter password:")
 
-    if (username == "grace" and password == "hopper") or /
-        (username == "emma" and password == "haskell"):
+    if (name == "grace" and password == "hopper") or (name == "emma" and password == "haskell"):
         print("You have succefffully logged in!")
     else:
         print("Incorrect username or password!")
